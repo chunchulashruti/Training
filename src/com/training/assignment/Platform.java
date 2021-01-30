fc3507348f5ed5d0606c76987d10fa30a1391714
Chandrashekar <vchandrashekar@outlook.com>
2021-01-30 14:38:54 +0530

    Added flows for create and list employees

--- src/com/training/assignment/Platform.java
+++ src/com/training/assignment/Platform.java
@@ -1,18 +1,74 @@
 package com.training.assignment;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Platform {
     private static Scanner scanner = new Scanner(System.in);
+    private static ArrayList<Employee> employees = new ArrayList();
+    private static int index = 0;
 
     public static void main(String[] args) {
-        System.out.println("Provide Employee details!");
 
+        while (true) {
+            int userChoice = askUsersChoice();
+            processUserChoice(userChoice);
+        }
+    }
+
+    private static void processUserChoice(int userChoice) {
+        switch (userChoice) {
+            case 1:
+                final Employee employee = createEmployee();
+                employees.add(employee);
+                break;
+            case 2:
+                // TODO: Update employee flow
+                System.out.println("Sorry ! update Employee is not yet supported!");
+                break;
+            case 3:
+                System.out.println("Sorry ! Delete Employee is not yet supported!");
+                // TODO: Delete employee flow
+                break;
+            case 4:
+                for (Employee e : employees) {
+                    e.displayInformation();
+                }
+                break;
+
+        }
+    }
+
+    private static int askUsersChoice() {
+        showUsersChoice();
+        showAChoice("Choose your choice!");
+        return scanner.nextInt();
+    }
+
+    private static void showUsersChoice() {
+        showChoices("1. Create Employee", "2. Update Employee", "3. Delete Employee", "4. List Employees");
+    }
+
+    private static void showAChoice(String s) {
+        System.out.println(s);
+    }
+
+    private static void showChoices(String... elements) {
+        for (String element : elements) {
+            showAChoice(element);
+        }
+    }
+
+
+    private static Employee createEmployee() {
+        showAChoice("Provide Employee details!");
         final String name = takeName();
         final int salary = takeSalary();
         final int age = takeAge();
         Employee employee = createEmployeeObject(name, salary, age);
         employee.displayInformation();
+
+        return employee;
     }
 
 
@@ -23,12 +79,12 @@ public class Platform {
     }
 
     private static int takeAge() {
-        System.out.println("Age:");
+        showAChoice("Age:");
         return scanner.nextInt();
     }
 
     private static String takeName() {
-        System.out.println("Name:");
+        showAChoice("Name:");
         final String name = scanner.next();
 
         if (!isNameValid(name))
@@ -39,14 +95,19 @@ public class Platform {
 
     private static boolean isNameValid(String name) {
         final boolean matches = name.matches("^[a-zA-Z]*$");
-        if (!matches)
-            System.out.println("Your name looks like invalid!Can you enter again!");
+        if (!matches) {
+            askForNameAgain();
+        }
 
         return matches;
     }
 
+    private static void askForNameAgain() {
+        System.out.println("Your name looks like invalid!Can you enter again!");
+    }
+
     private static int takeSalary() {
-        System.out.println("Salary:");
+        showAChoice("Salary:");
         return scanner.nextInt();
     }
 }
